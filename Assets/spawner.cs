8ace7668c98cdda6534e0f9e98b2da30b6aafaff
GirayKaradeniz <giraykaradeniz@hotmail.com>
2021-05-10 14:06:02 +0300

    spawnerlar

---
+++ Assets/spawner.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawner : MonoBehaviour
+{
+    public Transform[] spawnPoints;
+    public GameObject[] soul;
+    int randomSpawnPoint, randomsoul;
+    public static bool spawnAllowed;
+
+    void Start()
+    {
+        spawnAllowed = true;
+        InvokeRepeating("SpawnSoul", 0f, 1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnSoul()
+    {
+        if(spawnAllowed)
+        {
+            randomSpawnPoint = Random.Range(0, spawnPoints.Length);
+            randomsoul = Random.Range(0, soul.Length);
+            Instantiate(soul[randomsoul], spawnPoints[randomSpawnPoint].position, Quaternion.identity);
+        }
+    }
+}
