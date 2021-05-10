d0e4b5f87caee6fd5dd6b5bb224816f05a896538
furkankupcu <51542849+furkankupcu@users.noreply.github.com>
2021-05-10 23:40:02 +0300

    UpdateEnemy

--- Assets/Boss Script/Boss_run.cs
+++ Assets/Boss Script/Boss_run.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Boss_run : StateMachineBehaviour
 
 {   Transform boss;
@@ -9,10 +10,12 @@ public class Boss_run : StateMachineBehaviour
     Rigidbody2D rb;
     public float speed = 2.5f;
     private Vector3 basPos;
+    public float attackRange = 3f;
 
     void Start()
     {
-        basPos = new Vector3(boss.position.x, boss.position.y, boss.position.z);
+        basPos = new Vector3(26f, 4.56f, 0.15f);
+        
     }
 
 
@@ -32,22 +35,25 @@ public class Boss_run : StateMachineBehaviour
 
         Vector3 target = new Vector3(anakarakter.position.x, anakarakter.position.y,anakarakter.position.z);
         Vector3 newPos= Vector3.MoveTowards(rb.position, target, speed * Time.fixedDeltaTime);
+        Vector3 hedef = Vector3.MoveTowards(rb.position, basPos, speed * Time.fixedDeltaTime);
 
-        if (boss.position.x - anakarakter.position.x < 4  ) {
-            if (anakarakter.position.x > 10 ) {
-                rb.MovePosition(newPos);
-            }
-            else
-            {
-                rb.MovePosition(basPos);
-            }
+        if ( Mathf.Abs(boss.position.x - anakarakter.position.x) < 4) {
+             rb.MovePosition(newPos);
         }
+
+        if (Vector2.Distance(anakarakter.position,rb.position) <= attackRange)
+        {
+            animator.SetTrigger("Attack"); 
+
+        }
+
+
     }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-    
+        animator.ResetTrigger("Attack");
     }
     
 }
